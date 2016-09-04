7f31350acc9de7715795e6e3f3bb80b5a227a46a
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2016-09-04 19:43:16 +0800

    Add new files.

---
+++ IntellijSwiftToDoList/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  IntellijSwiftToDoList
+//
+//  Created by Mohammad Nurdin bin Norazan on 04/09/2016.
+//  Copyright © 2016 Intellij System Solution Sdn. Bhd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
